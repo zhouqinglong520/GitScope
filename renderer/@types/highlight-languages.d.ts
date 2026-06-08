4871c1eef3332e99fb9df8d9b40df933b8b71bb5
zhouqinglong520 <44882914+zhouqinglong520@users.noreply.github.com>
2026-06-08 18:21:46 +0800

    feat: highlight-languages.d.ts - replace showInputBox with pro dialogs + recent repos + type declarations

---
+++ renderer/@types/highlight-languages.d.ts
@@ -0,0 +1,126 @@
+// highlight.js 语言模块类型声明
+declare module 'highlight.js/lib/languages/bash' {
+  import { LanguageFn } from 'highlight.js';
+  const language: LanguageFn;
+  export default language;
+}
+declare module 'highlight.js/lib/languages/c' {
+  import { LanguageFn } from 'highlight.js';
+  const language: LanguageFn;
+  export default language;
+}
+declare module 'highlight.js/lib/languages/cpp' {
+  import { LanguageFn } from 'highlight.js';
+  const language: LanguageFn;
+  export default language;
+}
+declare module 'highlight.js/lib/languages/csharp' {
+  import { LanguageFn } from 'highlight.js';
+  const language: LanguageFn;
+  export default language;
+}
+declare module 'highlight.js/lib/languages/css' {
+  import { LanguageFn } from 'highlight.js';
+  const language: LanguageFn;
+  export default language;
+}
+declare module 'highlight.js/lib/languages/diff' {
+  import { LanguageFn } from 'highlight.js';
+  const language: LanguageFn;
+  export default language;
+}
+declare module 'highlight.js/lib/languages/go' {
+  import { LanguageFn } from 'highlight.js';
+  const language: LanguageFn;
+  export default language;
+}
+declare module 'highlight.js/lib/languages/java' {
+  import { LanguageFn } from 'highlight.js';
+  const language: LanguageFn;
+  export default language;
+}
+declare module 'highlight.js/lib/languages/javascript' {
+  import { LanguageFn } from 'highlight.js';
+  const language: LanguageFn;
+  export default language;
+}
+declare module 'highlight.js/lib/languages/json' {
+  import { LanguageFn } from 'highlight.js';
+  const language: LanguageFn;
+  export default language;
+}
+declare module 'highlight.js/lib/languages/kotlin' {
+  import { LanguageFn } from 'highlight.js';
+  const language: LanguageFn;
+  export default language;
+}
+declare module 'highlight.js/lib/languages/markdown' {
+  import { LanguageFn } from 'highlight.js';
+  const language: LanguageFn;
+  export default language;
+}
+declare module 'highlight.js/lib/languages/php' {
+  import { LanguageFn } from 'highlight.js';
+  const language: LanguageFn;
+  export default language;
+}
+declare module 'highlight.js/lib/languages/plaintext' {
+  import { LanguageFn } from 'highlight.js';
+  const language: LanguageFn;
+  export default language;
+}
+declare module 'highlight.js/lib/languages/python' {
+  import { LanguageFn } from 'highlight.js';
+  const language: LanguageFn;
+  export default language;
+}
+declare module 'highlight.js/lib/languages/ruby' {
+  import { LanguageFn } from 'highlight.js';
+  const language: LanguageFn;
+  export default language;
+}
+declare module 'highlight.js/lib/languages/rust' {
+  import { LanguageFn } from 'highlight.js';
+  const language: LanguageFn;
+  export default language;
+}
+declare module 'highlight.js/lib/languages/scss' {
+  import { LanguageFn } from 'highlight.js';
+  const language: LanguageFn;
+  export default language;
+}
+declare module 'highlight.js/lib/languages/shell' {
+  import { LanguageFn } from 'highlight.js';
+  const language: LanguageFn;
+  export default language;
+}
+declare module 'highlight.js/lib/languages/sql' {
+  import { LanguageFn } from 'highlight.js';
+  const language: LanguageFn;
+  export default language;
+}
+declare module 'highlight.js/lib/languages/swift' {
+  import { LanguageFn } from 'highlight.js';
+  const language: LanguageFn;
+  export default language;
+}
+declare module 'highlight.js/lib/languages/typescript' {
+  import { LanguageFn } from 'highlight.js';
+  const language: LanguageFn;
+  export default language;
+}
+declare module 'highlight.js/lib/languages/xml' {
+  import { LanguageFn } from 'highlight.js';
+  const language: LanguageFn;
+  export default language;
+}
+declare module 'highlight.js/lib/languages/yaml' {
+  import { LanguageFn } from 'highlight.js';
+  const language: LanguageFn;
+  export default language;
+}
+declare module 'highlight.js/lib/languages/dart' {
+  import { LanguageFn } from 'highlight.js';
+  const language: LanguageFn;
+  export default language;
+}
